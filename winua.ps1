0838a9fe67d43ac6c86368426eaf0b272aa3e94c
Christian M. Casutt <cc@solvia.ch>
2023-06-10 12:38:35 +0200

    Update winua.ps1

--- winua.ps1
+++ winua.ps1
@@ -5,6 +5,15 @@
 #
 # This script is used to finalize the setup of a Windows computer
 
+# Setup error handling.
+Trap {
+    $_
+    Exit 1
+}
+$ErrorActionPreference = "Stop"
+
+Write-Host "Hi!"
+
 # Get the ID and security principal of the current user account
 $myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $myWindowsPrincipal = new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
@@ -17,4 +26,19 @@ if (-Not $myWindowsPrincipal.IsInRole($adminRole)) {
     Write-Output "ERROR: You need elevated Administrator privileges in order to run this script."
     Write-Output "       Start Windows PowerShell by using the Run as Administrator option."
     Exit 2
+}
+
+$EventSource = $MyInvocation.MyCommand.Name
+If (-Not $EventSource) {
+    $EventSource = "Powershell CLI"
+}
+
+If ([System.Diagnostics.EventLog]::Exists('Application') -eq $False -or [System.Diagnostics.EventLog]::SourceExists($EventSource) -eq $False) {
+    New-EventLog -LogName Application -Source $EventSource
+}
+
+# Detect PowerShell version.
+If ($PSVersionTable.PSVersion.Major -lt 3) {
+    Write-ProgressLog "PowerShell version 3 or higher is required."
+    Throw "PowerShell version 3 or higher is required."
 }
